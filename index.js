4c6655266ee7365528ff83e91481bda58c2395cd
Seth Deegan <jayandseth@gmail.com>
2018-09-03 16:23:02 -0500

    v0.0.2

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const port = process.env.PORT;
+
+
+// Server keeps the bot up with Uptime Robot pinging it
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('server requested');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`);
+});
